70c8eaa13551fe71aeab3a5e54fe879b5b0c4b85
sikiryl <saritrat.206@gmail.com>
2023-12-09 15:47:49 +0700

    feat; add lgtm propagator

--- trace/propagation-context/complete-lgtm/common.py
+++ trace/propagation-context/complete-lgtm/common.py
@@ -6,6 +6,36 @@ from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExport
 from opentelemetry.semconv.resource import ResourceAttributes
 from opentelemetry.semconv.trace import SpanAttributes
 from local_machine_resource_detector import LocalMachineResourceDetector
+import logging
+from opentelemetry.sdk._logs.export import ConsoleLogExporter, BatchLogRecordProcessor
+from opentelemetry.sdk._logs import (
+    LoggerProvider,
+    LoggingHandler,
+)
+
+
+def configure_logger(name, version):
+    local_resource = LocalMachineResourceDetector().detect()
+    resource = local_resource.merge(
+        Resource.create(
+            {
+                ResourceAttributes.SERVICE_NAME: name,
+                ResourceAttributes.SERVICE_VERSION: version,
+            }
+        )
+    )
+    provider = LoggerProvider(resource=resource)
+    # set_log_emitter_provider(provider)
+    exporter = ConsoleLogExporter()
+    provider.add_log_record_processor(BatchLogRecordProcessor(exporter))
+    logger = logging.getLogger(name)
+    handler = LoggingHandler(level=logging.DEBUG, logger_provider=provider)
+    logger.addHandler(handler)
+    return logger
+
+
+def configure_meter(name, version):
+    pass
 
 
 def configure_tracer(name, version):
@@ -25,6 +55,7 @@ def configure_tracer(name, version):
     trace.set_tracer_provider(provider)
     return trace.get_tracer(name, version)
 
+
 def set_span_attributes_from_flask():
     span = trace.get_current_span()
     span.set_attributes(
@@ -38,3 +69,7 @@ def set_span_attributes_from_flask():
             SpanAttributes.HTTP_CLIENT_IP: request.remote_addr,
         }
     )
+
+
+def start_recording_memory_metrics():
+    pass
